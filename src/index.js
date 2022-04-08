4df581c872f7d8c66b747e971ad3ceab23e0ab8e
Fandavies <93221294+Fandavies@users.noreply.github.com>
2022-04-08 15:17:54 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -8,3 +8,47 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+document.getElementById("slider").innerHTML = `
+<div class="container">
+<div class="showSlider fade">
+  <img src="img/i1.jpg" alt="Entering Library" />
+  <div class="contentBook">Enter into Library</div>
+</div>
+<div class="showSlider fade">
+  <img src="img/i2.jpg" alt="Select Book" />
+  <div class="contentBook">Select your book</div>
+</div>
+<div class="showSlider fade">
+  <img src="img/i3.jpg" alt="Registering ID" />
+  <div class="contentBook">Register your Library ID</div>
+</div>
+<div class="showSlider fade">
+  <img src="img/i4.jpg" alt="Returning Book" />
+  <div class="contentBook">Return your book after 7 days</div>
+</div>
+<a class="left" onclick="nextSlide(-1)">❮</a>
+<a class="right" onclick="nextSlide(1)">❯</a>
+</div>
+`;
+var slide_index = 1;
+slidesDisplay(slide_index);
+function nextSlide(n) {
+  slidesDisplay((slide_index += n));
+}
+function currentSlide(n) {
+  slidesDisplay((slide_index = n));
+}
+function slidesDisplay(n) {
+  var i;
+  var slides = document.getElementsByClassName("showSlider");
+  if (n > slides.length) {
+    slide_index = 1;
+  }
+  if (n < 1) {
+    slide_index = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slide_index - 1].style.display = "block";
+}

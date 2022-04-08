fa92c70fd04538e4e2502f093487280c6e488b6d
Fandavies <93221294+Fandavies@users.noreply.github.com>
2022-04-08 16:12:28 +0300

    45

--- src/index.js
+++ src/index.js
@@ -2,23 +2,25 @@ import "./styles.css";
 document.getElementById("slider").innerHTML = `
 <div class="container">
 <div class="showSlider fade">
-  <img src="img/i1.jpg" alt="Entering Library" />
-  <div class="contentBook">Enter into Library</div>
+  <img src="img/i1.jpg" alt="" />
+  <div class="contentBook">image4</div>
 </div>
 <div class="showSlider fade">
-  <img src="img/i2.jpg" alt="Select Book" />
-  <div class="contentBook">Select your book</div>
+  <img src="img/i2.jpg" alt="" />
+  <div class="contentBook">image4</div>
 </div>
 <div class="showSlider fade">
-  <img src="img/i3.jpg" alt="Registering ID" />
-  <div class="contentBook">Register your Library ID</div>
+  <img src="img/i3.jpg" alt="" />
+  <div class="contentBook">image4</div>
 </div>
 <div class="showSlider fade">
-  <img src="img/i4.jpg" alt="Returning Book" />
-  <div class="contentBook">Return your book after 7 days</div>
+  <img src="img/i4.jpg" alt="" />
+  <div class="contentBook">image4</div>
 </div>
-<a class="left" onclick="nextSlide(-1)">❮</a>
+<div>
+<a class="left" onclick='nextSlide(-1)'>❮</a>
 <a class="right" onclick="nextSlide(1)">❯</a>
+</div>
 </div>
 `;
 var slide_index = 1;
